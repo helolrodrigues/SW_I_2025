b83607b5d03f54747fd63210afe9166460cd739f
helolrodrigues <161508995+helolrodrigues@users.noreply.github.com>
2025-06-13 16:49:34 -0300

    Add files via upload

---
+++ aula13/usalivro.php
@@ -0,0 +1,33 @@
+<?php
+include_once 'Livro.class.php';
+
+$livro = new Livro ('Iracema', 'José de Alencar', true);
+
+//echo "<prep>"; //Ajusta para visualização de código
+//print_r($livro);
+//echo "</prep>";
+
+echo "STATUS DO LIVRO: ";
+echo $livro ->ExibirStatus();
+echo "<hr>";
+
+echo "Foi feito um empréstimo";
+$livro -> Emprestar();
+echo "<hr>";
+
+echo "STATUS DO LIVRO: ";
+echo $livro ->ExibirStatus();
+echo "<hr>";
+
+echo "Foi feito um empréstimo";
+$livro -> Emprestar();
+echo "<hr>";
+
+echo "Efetuando uma devolução";
+$livro ->Devolver();
+echo "<hr>";
+
+echo "STATUS DO LIVRO: ";
+echo $livro ->ExibirStatus();
+echo "<hr>";
+?>
